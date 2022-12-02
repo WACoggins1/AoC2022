8179b38cd47d98fc48707aa4184b12cf3cfcac6b
Anthony Coggins <90111467+WACoggins1@users.noreply.github.com>
2022-12-01 19:11:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+#read in .txt
 elfTxt = open('elfCalories.txt', 'r')
 
+#convert to something workable
 calories = elfTxt.read().strip().split('\n\n')
 
 elfCals = []
 
+#get a list of sums
 for elf in calories:
     meals = elf.split('\n')
     meals = [eval(i) for i in meals]
     elfCals.append(sum(meals))
 
+#get the max
 ans1 = max(elfCals)
 print("The elf carrying the most has", ans1)
 
+#order it and find the 3 largest sums.
 elfCals = sorted(elfCals)
 ans2 = sum(elfCals[-1:-4:-1])
 print("The sum of the top 3 is", ans2)
